02e45a865c14780270887757d8a9c5e8e4437157
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-09-22 10:45:24 +0200

    userRepository methods

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -23,6 +24,9 @@ class UsersRepository {
   };
 
   async create(attrs){
+
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
     records.push(attrs);
 
@@ -33,13 +37,59 @@ class UsersRepository {
   async writeAll(records){
     await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
   }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOne(id){
+    const records = await this.getAll();
+    return records.find(records => records.id === id);
+  }
+
+  async delete(id){
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs){
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+    if(!record){
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters){
+    const records = await this.getAll();
+
+    for (let record of records){
+      let found = true;
+      for (let key in filters){
+        if (record[key] !== filters[key]){
+          found = false;
+        }
+      } 
+    if(found){
+      return record;
+    }  
+  }
+}
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-   await repo.create({email : 'test3@test.com', password: 'password'});
-  const users = await repo.getAll();
-  console.log(users);
+  // await repo.create({email: 'meemai@lala.caom', password : 'pwd123'});
+
+  // await repo.update('1231231', {password: 'mynewPWD'});
+
+  const user = await repo.getOneBy({email: 'meemai@lala.caom', password: 'mynewPsWD'});
+
+  console.log(user);
 }
 
 test();
