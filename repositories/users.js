9a39e03c940540c8b54ab22d609174b832114a75
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-09-22 15:27:37 +0200

    production level authentication

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util  = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -24,14 +27,31 @@ class UsersRepository {
   };
 
   async create(attrs){
-
     attrs.id = this.randomId();
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
 
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`
+    }
+    records.push(record);
 
     // write the updated 'records' array back to this.filename
     await this.writeAll(records);
+
+    return record;
+  }
+
+  async comparePasswords(saved, supplied){
+    // Save -> pwd saved in out database . 'hashed.salt'
+    // Supplied -> password given to us by a user trying to sign in
+
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records){
@@ -81,15 +101,4 @@ class UsersRepository {
 }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-  // await repo.create({email: 'meemai@lala.caom', password : 'pwd123'});
-
-  // await repo.update('1231231', {password: 'mynewPWD'});
-
-  const user = await repo.getOneBy({email: 'meemai@lala.caom', password: 'mynewPsWD'});
-
-  console.log(user);
-}
-
-test();
+module.exports = new UsersRepository('users.json');
