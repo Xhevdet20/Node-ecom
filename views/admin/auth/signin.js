9538e662770ae1304d928fdeb7372b8024beeb8d
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-09-23 09:37:47 +0200

    admin layout

---
+++ views/admin/auth/signin.js
@@ -0,0 +1,15 @@
+const layout = require('../layout');
+
+module.exports = () => {
+  return layout ({ 
+    content : `
+      <div>
+        <form method="post">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+  `
+  });
+}
